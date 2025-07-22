70b884b009a176e29641667c50edf5fd4f7a8235
TelRich <okorogoodrich@gmail.com>
2025-07-22 14:05:14 +0100

    started project

---
+++ terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+    required_providers {
+        google = {
+            source = "hashicorp/google"
+            version = "~> 5.0"
+        }
+    }
+
+    required_version = ">= 1.3.0"
+}
+
+provider "google" {
+    credentials = file("C:/Users/okoro/AppData/Local/Google/Cloud SDK/key.json")
+    project = var.project_id
+    region = var.region
+}
+
+resource "google_storage_bucket" "raw_data_bucket" {
+    name = var.bucket_name
+    location = var.region
+    force_destroy = true
+
+    uniform_bucket_level_access = true
+}
